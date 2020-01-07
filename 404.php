dfddccf66046e29a88dfced4a8e4a94472f59c93
Nikolas Haug <nikohaug21@gmail.com>
2020-01-07 15:29:37 -0600

    add 404 styles

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div class="not-found__container">
+    <div class="not-found__text">
+        <h1>404 Not Found</h1>
+        <a href="<?php echo site_url('') ?>">Go Back</a>
+    </div>
+</div>
+
+<?php get_footer(); ?>
